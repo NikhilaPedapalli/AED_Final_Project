77723ce4bac170d10d7b94c1d3e6d61b83224309
KavyaMannem <mannem.k@northeastern.edu>
2021-12-11 20:43:46 -0500

    Adding shelter organization

--- src/Business/Organization/ShelterOrganizationDirectory.java
+++ src/Business/Organization/ShelterOrganizationDirectory.java
@@ -4,10 +4,34 @@
  */
 package Business.Organization;
 
+import Business.Organization.Organization.Type;
+import java.util.ArrayList;
+
 /**
  *
  * @author pinky
  */
 public class ShelterOrganizationDirectory {
     
+    private ArrayList<ShelterOrganization> organizationList;
+
+    public ShelterOrganizationDirectory() {
+        organizationList = new ArrayList();
+    }
+
+    public ArrayList<ShelterOrganization> getOrganizationList() {
+        return organizationList;
+    }
+    public void deleteShelterOrgInfo(Organization v)
+    {
+        organizationList.remove(v);
+    }
+    public Organization createOrganization(Type type){
+        Organization organization = null;
+        if (type.getValue().equals(Type.Shelter.getValue())){
+            organization = new ShelterOrganization();
+            organizationList.add((ShelterOrganization) organization);
+        }
+        return organization;
+    }
 }
