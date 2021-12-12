31550ac7bc8c516131792f5dd46c03689575cf79
Srihitha Kilari <90711965+srihithakilari15@users.noreply.github.com>
2021-12-11 20:58:24 -0500

    Adding Enterprise Directory

--- src/Business/Enterprise/EnterpriseDirectory.java
+++ src/Business/Enterprise/EnterpriseDirectory.java
@@ -4,10 +4,50 @@
  */
 package Business.Enterprise;
 
+import Business.Organization.OrganizationDirectory;
+import java.util.ArrayList;
+
 /**
  *
  * @author pinky
  */
 public class EnterpriseDirectory {
+
+    private ArrayList<Enterprise> enterpriseList;
+   
+
+    public ArrayList<Enterprise> getEnterpriseList() {
+        return enterpriseList;
+    }
+
+    public void setEnterpriseList(ArrayList<Enterprise> enterpriseList) {
+        this.enterpriseList = enterpriseList;
+    }
     
+    public void deleteEnterInfo(Enterprise e)
+    {
+        enterpriseList.remove(e);
+    }
+    
+    public EnterpriseDirectory(){
+        enterpriseList=new ArrayList<Enterprise>();
+    }
+    
+    //Create enterprise
+    public Enterprise createAndAddEnterprise(String name,Enterprise.EnterpriseType type){
+        Enterprise enterprise=null;
+        if(type==Enterprise.EnterpriseType.Hospital){
+            enterprise=new HospitalEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        else if(type==Enterprise.EnterpriseType.PetStore){
+            enterprise=new PetStoreEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        else if(type==Enterprise.EnterpriseType.Shelter){
+            enterprise=new ShelterEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        return enterprise;
+    }
 }
