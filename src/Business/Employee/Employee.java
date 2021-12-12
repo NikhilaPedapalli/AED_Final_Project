b80a17a66f66c747bf1bbdc6ae524e84ed2fd563
NikhilaPedapalli <90415525+NikhilaPedapalli@users.noreply.github.com>
2021-12-12 01:49:26 -0500

    Added User Interface

--- src/Business/Employee/Employee.java
+++ src/Business/Employee/Employee.java
@@ -1,6 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
 package Business.Employee;
@@ -10,5 +9,33 @@ package Business.Employee;
  * @author Nikhila
  */
 public class Employee {
+    
+    private String name;
+    private int id;
+    private static int count = 1;
+
+    public Employee() {
+        id = count;
+        count++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+    
     
 }
